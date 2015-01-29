a834fa79921afc745b73c988af4a6579941774d0
Mixer Gutierrez <mixergutierrez@gmail.com>
2015-01-29 13:55:57 -0500

    Fix survey reference in survey response controller create

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -13,7 +13,7 @@ class SurveyResponsesController < ApplicationController
   def create
     answers = params[:survey_responses][:questions_answers]
     answers.each do |question, answer|
-      SurveyResponse.create(survey_id: params[:survey_id], question_id: question, answer_id: answer, user_id: current_user.id, timestamp: Time.now.to_i)
+      SurveyResponse.create(survey_id: @survey.id, question_id: question, answer_id: answer, user_id: current_user.id, timestamp: Time.now.to_i)
     end
     #NotificationsMailer.new_survey_response_email(@survey).deliver
     render :thanks
